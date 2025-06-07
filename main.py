cc93578b83761554d39e504613c410ab97c5297c
Goway-Li <dlzgwer@outlook.com>
2025-06-08 01:34:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -25,9 +25,20 @@ def gen_normal_like(n):
     val = int(round(mean + sigma * z))
     return max(0, min(n - 1, val))
 
+# 输出测试数据文件
+def dump_binary_file():
+    try:
+        with open("randomdata.bin", "wb") as f:
+            total_bits = 20000
+            byte_cnt = total_bits // 8
+            data = bytes([gen_mapped_uniform(256) for _ in range(byte_cnt)])
+            f.write(data)
+        print("测试数据文件已保存。\n")
+    except Exception as ex:
+        print(f"文件写入出错: {ex}\n")
+
 # 主函数
 def main():
-
     generators = [
         ("rand()%N", gen_basic),
         ("均匀分布", gen_mapped_uniform),
@@ -41,6 +52,9 @@ def main():
         testtool.analyze_pattern_test(N, num_samples, func)
         testtool.analyze_run_count(N, num_samples, func)
         testtool.analyze_third_diff(N, num_samples, func)
+
+    print("生成随机数二进制文件...")
+    dump_binary_file()
 
     print("所有测试完成。")
 
